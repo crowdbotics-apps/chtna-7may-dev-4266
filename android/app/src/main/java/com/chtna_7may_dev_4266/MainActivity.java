25deb0f7524cada69117d6b422235a0406cdf46c
Crowdbotics <team@crowdbotics.com>
2020-05-07 07:25:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chtna_7may_dev_4266/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chtna_7may_dev_4266;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "chtna_7may_dev_4266";
+    }
+}
